b757bab15913c7527fb1d3b94be9de8862ce4dd0
pathiggins13 <pat_higgins@hotmail.com>
2015-08-19 15:21:11 -0600

    integrates table with party data

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -21,56 +21,7 @@ angular.module('utahvotesApp', ['googlechart'])
 		Other : 0
 	};
 
-	var chart1 = {};
-    chart1.type = "ColumnChart";
-    chart1.cssStyle = "height:200px; width:300px;";
-    chart1.data = {"cols": [
-        {id: "month", label: "Month", type: "string"},
-        {id: "laptop-id", label: "Laptop", type: "number"},
-        {id: "desktop-id", label: "Desktop", type: "number"},
-        {id: "server-id", label: "Server", type: "number"},
-        {id: "cost-id", label: "Shipping", type: "number"}
-    ], "rows": [
-        {c: [
-            {v: "January"},
-            {v: 19, f: "42 items"},
-            {v: 12, f: "Ony 12 items"},
-            {v: 7, f: "7 servers"},
-            {v: 4}
-        ]},
-        {c: [
-            {v: "February"},
-            {v: 13},
-            {v: 1, f: "1 unit (Out of stock this month)"},
-            {v: 12},
-            {v: 2}
-        ]},
-        {c: [
-            {v: "March"},
-            {v: 24},
-            {v: 0},
-            {v: 11},
-            {v: 6}
-
-        ]}
-    ]};
-
-    chart1.options = {
-        "title": "Sales per month",
-        "isStacked": "true",
-        "fill": 20,
-        "displayExactValues": true,
-        "vAxis": {
-            "title": "Sales unit", "gridlines": {"count": 6}
-        },
-        "hAxis": {
-            "title": "Date"
-        }
-    };
-
-    chart1.formatters = {};
-
-    $scope.chart = chart1;
+	
 
 
 
@@ -108,6 +59,48 @@ angular.module('utahvotesApp', ['googlechart'])
 
 			});
 		    console.log(vanimport)
-			console.log(party)
+			console.log(party.Democrats)
+			var chart1 = {};
+    chart1.type = "PieChart";
+    chart1.cssStyle = "height:500px; width:500px;";
+    chart1.data = {"cols": [
+        {id: "race", label: "Race", type: "string"},
+        {id: "people", label: "People", type: "number"},
+        {id: "desktop-id", label: "Desktop", type: "number"},
+        {id: "server-id", label: "Server", type: "number"},
+        {id: "cost-id", label: "Shipping", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "Democrats"},
+            {v: party.Democrats},
+            
+        ]},
+        {c: [
+            {v: "Republicans"},
+            {v: party.Republicans},
+        ]},
+        {c: [
+            {v: "Other"},
+            {v: party.Other}
+        ]}
+    ]};
+
+    chart1.options = {
+        "title": "Race",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+            "title": "Sales unit", "gridlines": {"count": 6}
+        },
+        "hAxis": {
+            "title": "Date"
+        }
+    };
+
+    chart1.formatters = {};
+
+    $scope.chart = chart1;
+
 	});
 });
