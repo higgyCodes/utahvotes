dfb3843e6d61b233ecc7460665a2b06150514367
pathiggins13 <pat_higgins@hotmail.com>
2015-08-24 10:37:21 -0600

    adds map with hardcoded data

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -81,10 +81,8 @@ angular.module('utahvotesApp', ['googlechart', 'ngMap'])
 		'2 of 3' : 0,
 		'3 of 3' : 0
 	}
-	var taxiData = [
-  new google.maps.LatLng(37.782551, -122.445368),
-
-];
+	
+	
 
 
     $scope.$on('packagedeal', function(event, data) {
@@ -616,10 +614,42 @@ angular.module('utahvotesApp', ['googlechart', 'ngMap'])
 
 		    voterpropchart.formatters = {};
 
-		    var heatmap;
+		   var heatmap;
   $scope.$on('mapInitialized', function(event, map) {
     heatmap = map.heatmapLayers.foo;
   });
+
+  $scope.toggleHeatmap= function(event) {
+    heatmap.setMap(heatmap.getMap() ? null : $scope.map);
+  };
+
+  $scope.changeGradient = function() {
+    var gradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+  }
+
+  $scope.changeRadius = function() {
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+  }
+
+  $scope.changeOpacity = function() {
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+  }
 
 
 
@@ -644,4 +674,42 @@ angular.module('utahvotesApp', ['googlechart', 'ngMap'])
 
 
 			});
-	});
+	})
+.controller('LayerHeatmapCtrl', function($scope) {
+  var heatmap;
+  $scope.$on('mapInitialized', function(event, map) {
+    heatmap = map.heatmapLayers.foo;
+  });
+
+  $scope.toggleHeatmap= function(event) {
+    heatmap.setMap(heatmap.getMap() ? null : $scope.map);
+  };
+
+  $scope.changeGradient = function() {
+    var gradient = [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+  }
+
+  $scope.changeRadius = function() {
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+  }
+
+  $scope.changeOpacity = function() {
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+  }
+});
