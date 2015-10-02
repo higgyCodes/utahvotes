32ac325590ea83437bf87759aad83926ba9a7ac9
pathiggins13 <pat_higgins@hotmail.com>
2015-10-02 12:42:28 -0600

    starts modulation of code, creates services

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -8,7 +8,10 @@
  * Controller of the utahvotesApp
  */
 angular.module('utahvotesApp', ['googlechart', 'ngMap'])
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, dataService) {
+
+
+  	dataService.testrun()
 
   	var testrun = []
 	var party = {
@@ -101,28 +104,11 @@ angular.module('utahvotesApp', ['googlechart', 'ngMap'])
     $scope.$on('packagedeal', function(event, data) {
 	   	var vanimport = d3.tsv.parse(data);
 
-		for (var i = 0; i < vanimport.length; i++) {
-			var modifications = vanimport[i]
-		    modifications["Activist"] = modifications["2012:Catalist:GenAct"];
-		    modifications["Partisanship"] = modifications["2012:PartisanshipScr"];
-		    modifications["VoterProp"] = modifications["2013:Cat:VotePropv2"];
-		    if (modifications["RaceName "]) {
-		    	modifications["Race"] = modifications["RaceName "]	
-		    } else {modifications["Race"] = modifications["RaceName"]}
-		    delete modifications["2012:Catalist:GenAct"];
-		    delete modifications["2012:PartisanshipScr"];
-		    delete modifications["2013:Cat:VotePropv2"]
-		};
+		dataService.nameModifications(vanimport)
 
-		    // Changing strings to data
-		vanimport.forEach(function(d){
-		   	d.Age = +d.Age
-		    d.Ideology = +d.Ideology
-		    d.Activist = +d.Activist
-		    d.Partisanship = +d.Partisanship
-		    d.VoterProp = +d.VoterProp
-		});
+		dataService.stringToData(vanimport)
 
+		console.log(vanimport)
 		    // creating party data set
 		vanimport.forEach(function(d){
 		    	//Party
---
+++ scripts/services/dataService.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('utahvotesApp')
+  .factory('dataService', function () {
+  	var service = {};
+
+service.testrun = function() {
+	console.log("This is a test")
+}
+
+//Modifies names so that it's easily read
+//Can add to this if there is additional variances
+service.nameModifications = function(vanimport) {
+	for (var i = 0; i < vanimport.length; i++) {
+				var modifications = vanimport[i]
+			    modifications["Activist"] = modifications["2012:Catalist:GenAct"];
+			    modifications["Partisanship"] = modifications["2012:PartisanshipScr"];
+			    modifications["VoterProp"] = modifications["2013:Cat:VotePropv2"];
+			    if (modifications["RaceName "]) {
+			    	modifications["Race"] = modifications["RaceName "]	
+			    } else {modifications["Race"] = modifications["RaceName"]}
+			    delete modifications["2012:Catalist:GenAct"];
+			    delete modifications["2012:PartisanshipScr"];
+			    delete modifications["2013:Cat:VotePropv2"]
+			return vanimport
+			};
+	};
+
+//Changes Strings to data
+service.stringToData = function(vanimport) {
+	vanimport.forEach(function(d){
+		   	d.Age = +d.Age
+		    d.Ideology = +d.Ideology
+		    d.Activist = +d.Activist
+		    d.Partisanship = +d.Partisanship
+		    d.VoterProp = +d.VoterProp
+		});
+	return vanimport
+}
+
+return service;
+      })
